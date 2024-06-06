11f8cff4ba36da5524df2cba871495b6fadd65c8
MikeJ28 <tungvshe176052@fpt.edu.vn>
2024-06-06 21:49:35 +0700

    update #19: UI quản lý lương admin

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -68,7 +68,16 @@ export const baseQueryWithInterceptor = async (args, api, extraOptions) => {
 export const api = createApi({
   baseQuery: baseQueryWithInterceptor,
   reducerPath: 'api',
-  tagTypes: ['FAVORITE', 'STATUS', 'EmployeesTag', 'AccountsTag', 'AccountOptionsTag', 'GeneralsTag', 'MenuInfosTag', 'SalariesTag'],
+  tagTypes: [
+    'FAVORITE',
+    'STATUS',
+    'EmployeesTag',
+    'AccountsTag',
+    'AccountOptionsTag',
+    'GeneralsTag',
+    'MenuInfosTag',
+    'SalariesTag'
+  ],
   extractRehydrationInfo(action, { reducerPath }) {
     if (action.type === REHYDRATE) {
       return action.payload?.[reducerPath]
